68f3d7f9afeac90d14d879f4364363cb1a44fcba
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-06 18:09:48 -0700

    Initiating with the game

--- madlibs.py
+++ madlibs.py
@@ -41,6 +41,28 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def play_game():
+    """Plays a game"""
+
+    play = request.args.get("play")
+    if play=="no":
+        return render_template("goodbye.html")
+    else:
+        return render_template("game.html")
+
+@app.route('/madlib')
+def show_madlib():
+
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html", person=person, color=color,
+        noun=noun, adjective=adjective)
+
+
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
